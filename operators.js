7bc6dda79bf27f727cc8d5cf5e8cabd5ca3cbb81
raman <ramanshankar24794@gmail.com>
2026-04-24 22:09:55 +0530

    this is my first commit. created a simple js program. also created if else and ternary operator

---
+++ operators.js
@@ -0,0 +1,13 @@
+let score=85;
+
+if(score>=90)
+    {console.log("Grade A");}
+else if(score>=80)
+    {console.log("Grade B");}
+else 
+    {console.log("Grade c");}
+
+score ="a";
+
+result = score >=90 ? "Grade A" : score>=80 ? "Grade B": score<80 ? "Grade C":"INVALID";
+console.log(result);
---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("This is my first Javascript program")
+
+function test(){
+    var temp=5;
+    console.log (typeof temp);
+    temp="abc";
+    console.log (typeof temp);
+    temp=true;
+    console.log (typeof temp);
+}
+test();
+
+let a = "10a";
+let b = 20;
+let c = a + b;
+let d = a - b;
+console.log(c);
+console.log(d);
+
+let count=5;
+let count1=++count;
+let count2=count++;
+
+console.log(count);
+console.log(count1);
+console.log(count2);
